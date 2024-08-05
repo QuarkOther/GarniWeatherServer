019af7d1d700f24ec709b65632e38e2cb47aaca6
Jan Palička <jan.palicka@firma.seznam.cz>
2024-08-05 15:06:02 +0200

    feat: add socket read

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("hello world from main.py")
+from common.config_load import SERVER_IP, SERVER_PORT
+from socketDataGet.getSocketData import socket_data_receive
+
+
+def __main__():
+    socket_data_receive(SERVER_IP, SERVER_PORT)
+
+
+if __name__ == "__main__":
+    __main__()
